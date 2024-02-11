8a83a65015ca16059679bcda934934bbd4b5d9c0
bananainfp <159679046+bananainfp@users.noreply.github.com>
2024-02-11 14:31:44 +0800

    script.js

--- script.js
+++ script.js
@@ -43,11 +43,11 @@ function resizeYesButton() {
 function generateMessage(noCount) {
   const messages = [
     "No",
-    "Are you sure?",
-    "Pookie please",
-    "Don't do this to me :(",
-    "You're breaking my heart",
-    "I'm gonna cry...",
+    "Are you sure Tetaa?",
+    "Mau aja dongg",
+    "Jangan gitu lahh :(",
+    "Mau yaa",
+    "Sedih nih aku...",
   ];
 
   const messageIndex = Math.min(noCount, messages.length - 1);
